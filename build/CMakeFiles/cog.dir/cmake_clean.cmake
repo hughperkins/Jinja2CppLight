28bdaf81fe47880f14608012644a3f6b985e95b1
Maximilian Haupt <mail@maximilianhaupt.com>
2016-04-10 17:57:45 +0200

    Remove build artifacts from repo.

--- build/CMakeFiles/cog.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/cog"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/cog.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
